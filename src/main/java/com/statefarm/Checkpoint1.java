a186c41ec783f23ce6d5b8f059f9716ef6aeeb07
Christian <cepps646@gmail.com>
2021-08-17 11:23:16 -0400

    created base syntax for if and for loop

--- src/main/java/com/statefarm/Checkpoint1.java
+++ src/main/java/com/statefarm/Checkpoint1.java
@@ -2,5 +2,32 @@ package com.statefarm;
 
 public class Checkpoint1 {
 
+    public static void main(String[] args) {
+        int sum = 0;
+        if (args.length < 1) {
+            System.out.println(0);
+        } else if (args.length > 0){
+            for (var i = 0; i < args.length; i++) {
+
+                if (i >= 0) {
+                    int x = sum[i] + sum;
+
+                }
+
+
+                }
+
+
+                if(args.length < 5  % 2 == 0)
+            System.out.println(0);
+        else
+            System.out.println(num + " is odd");
+
+
+
+
+    }
+
+
 
 }
