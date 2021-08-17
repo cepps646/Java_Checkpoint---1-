f6571471929767a8e264b4260264883843297ac4
Kayla Swartzlander <swartzlander20@gmail.com>
2021-08-17 10:59:33 -0500

    working code that adds positive integers

--- src/main/java/com/statefarm/Checkpoint1.java
+++ src/main/java/com/statefarm/Checkpoint1.java
@@ -6,28 +6,14 @@ public class Checkpoint1 {
         int sum = 0;
         if (args.length < 1) {
             System.out.println(0);
-        } else if (args.length > 0){
-            for (var i = 0; i < args.length; i++) {
-
-                if (i >= 0) {
-                    int x = sum[i] + sum;
-
+        } else if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                int value = Integer.parseInt(args[i]);
+                if (value >= 0) {
+                    sum = value + sum;
                 }
-
-
-                }
-
-
-                if(args.length < 5  % 2 == 0)
-            System.out.println(0);
-        else
-            System.out.println(num + " is odd");
-
-
-
-
+            }
+            System.out.println("Outside for:" + sum);
+        }
     }
-
-
-
 }
